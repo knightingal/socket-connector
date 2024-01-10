1b5e9863a76fcf44aba8f6ff2a109116a60aa059
knightingal <yujianming1985@hotmail.com>
2024-01-10 20:41:06 +0800

    loop send and recv

--- src/test/kotlin/SocketConnector.kt
+++ src/test/kotlin/SocketConnector.kt
@@ -8,6 +8,7 @@ import kotlin.test.Test
 class SocketConnector {
     val bodyString = "what is your name"
 
+    @OptIn(ExperimentalStdlibApi::class)
     @Test
     fun socketConnect() {
         val socket = Socket("127.0.0.1", 8080)
@@ -15,18 +16,28 @@ class SocketConnector {
             .header("user-agent", "Thunder Client (https://www.thunderclient.com)")
             .header("Host", "127.0.0.1:8080")
             .header("content-type", "text/plain")
-            .header("content-length", "${bodyString.length}")
-            .body(bodyString)
+            .header("Transfer-Encoding", "chunked")
 
 
         val inputStream = socket.getInputStream()
         val outputStream = socket.getOutputStream()
         val requestMessage = request.toMessage()
         outputStream.write(requestMessage.toByteArray())
+        Thread {
+            for (i in 1..3) {
+                outputStream.write("${bodyString.length.toHexString()}\r\n".toByteArray())
+                outputStream.write("$bodyString\r\n".toByteArray())
+                outputStream.flush()
+                Thread.sleep(5 * 1000)
+            }
+            outputStream.write("0\r\n\r\n".toByteArray())
+            outputStream.flush()
+        }.start()
         while(true) {
             val b = ByteArray(128)
             val readLen = inputStream.read(b)
             if (readLen <= 0) {
+                println("breaked")
                 break
             }
             println(String(b, 0, readLen))
