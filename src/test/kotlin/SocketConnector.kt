f0f1bfc0362e519ac855e4c459d2a2b0195787f2
knightingal <yujianming1985@hotmail.com>
2024-01-10 21:02:44 +0800

    multi thread send

--- src/test/kotlin/SocketConnector.kt
+++ src/test/kotlin/SocketConnector.kt
@@ -6,11 +6,25 @@ import java.net.Socket
 import kotlin.test.Test
 
 class SocketConnector {
-    val bodyString = "what is your name"
+    val bodyString = "what is your name:"
 
-    @OptIn(ExperimentalStdlibApi::class)
     @Test
     fun socketConnect() {
+        val thread1 = Thread {
+            socketConnectByName("user1")
+        }
+        val thread2 = Thread {
+            socketConnectByName("user2")
+        }
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread2.join()
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    fun socketConnectByName(name: String) {
+        val msgContent = bodyString + name;
         val socket = Socket("127.0.0.1", 8080)
         val request = Request(Method.POST, "/examples/servlets/servlet/SocketConnectExample")
             .header("user-agent", "Thunder Client (https://www.thunderclient.com)")
@@ -25,8 +39,8 @@ class SocketConnector {
         outputStream.write(requestMessage.toByteArray())
         Thread {
             for (i in 1..3) {
-                outputStream.write("${bodyString.length.toHexString()}\r\n".toByteArray())
-                outputStream.write("$bodyString\r\n".toByteArray())
+                outputStream.write("${msgContent.length.toHexString()}\r\n".toByteArray())
+                outputStream.write("$msgContent\r\n".toByteArray())
                 outputStream.flush()
                 Thread.sleep(5 * 1000)
             }
@@ -40,9 +54,8 @@ class SocketConnector {
                 println("breaked")
                 break
             }
-            println(String(b, 0, readLen))
+            println("[$name]" + String(b, 0, readLen))
         }
-
 
     }
 
